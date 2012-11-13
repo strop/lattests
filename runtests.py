c6c9595e1e9eddc9ee6fff5e3e59c2ffdb09cf38
J <a@a.pl>
2012-11-13 10:57:01 +0100

    testy i-o

--- runtests.py
+++ runtests.py
@@ -1,12 +1,12 @@
+#!/usr/bin/env python
 import config
 import os
 import subprocess as subp
 
-
 pass_on_success = "pass_on_success"
 pass_on_error = "pass_on_error"
 
-def run(test_section_title, testdir, subdir, test_pass_type, print_compiler_output = True):
+def run(test_section_title, testdir, subdir, test_pass_type, print_compiler_output = False, run_io_phase = True):
   test_count = 0
   success_count = 0
   failed_test_files = []
@@ -19,24 +19,30 @@ def run(test_section_title, testdir, subdir, test_pass_type, print_compiler_outp
     if len(folder[2]) > 0:
       tests = set([x[0] for x in map(lambda x: x.split("."), folder[2])])
       for t in tests:
+        io_phase_succeeded = True
+        compilation_phase_succeeded = False
+        test_count = test_count + 1
+        test_file = t + config.suffix_src
+        input_file = t + config.suffix_in
+        output_file = t + config.suffix_out
+        test_src_path = folder[0] + test_file
+        test_bin_path = config.test_bin_dir + "/" + t
+        test_input_path = folder[0] + "/" + input_file
+        test_output_path = folder[0] + "/" + output_file
+
         if not os.path.exists(folder[0] + "/" + t + config.suffix_src):
-          print "Error: nie istnieje plik zrodlowy testu",folder[0],t
+          print "Blad: nie istnieje plik zrodlowy testu",folder[0],t
         else:
-          test_file = t + config.suffix_src
-          test_count = test_count + 1
-          test_src_path = folder[0] + test_file
-          test_bin_path = config.test_bin_dir + "/" + t
           print "Test:",test_src_path,
           compile_out = ""
           try:
             compile_out = subp.check_output([config.compiler_bin, test_src_path], stderr = subp.STDOUT)
             if test_pass_type == pass_on_success:
               print "OK"
-              success_count = success_count + 1
-              passed_test_files.append(test_file)
+              compilation_phase_succeeded = True
             else:
               print "FAILED"
-              failed_test_files.append(test_file)
+              compilation_phase_succeeded = False
             if print_compiler_output:
               print "Wyjscie z kompilacji:"
               print compile_out
@@ -47,19 +53,36 @@ def run(test_section_title, testdir, subdir, test_pass_type, print_compiler_outp
           except subp.CalledProcessError as non_zero:
             if test_pass_type == pass_on_error:
               print "OK"
-              success_count = success_count + 1
-              passed_test_files.append(test_file)
+              compilation_phase_succeeded = True
             else:
               print "FAILED"
-              failed_test_files.append(test_file)
+              compilation_phase_succeeded = False
             print "Kompilacja zakonczyla sie z kodem bledu", non_zero.returncode
             if print_compiler_output:
               print "Wyjscie z kompilacji:"
               print non_zero.output
             print
-          pass
+        # sprawdzanie i/o
+        if os.path.exists(test_input_path) and run_io_phase:
+          if not os.path.exists(test_output_path):
+            print "Blad: nie istnieje plik wynikowy dla pliku wejsciowego", input_file
+            output = subp.check_output("test_bin_path" + " < " + test_input_path, shell=True)
+            expected_output = file(test_output_path).read()
+            if output != expected_output:
+              print "FAILED"
+              print "oczekiwano:"
+              print expected_output
+              print "otrzymano:"
+              print output
+              io_phase_succeeded = False
+            else:
+              io_phase_succeeded = True
+        if ((not run_io_phase) or io_phase_succeeded) and compilation_phase_succeeded:
+          success_count = success_count + 1
+          passed_test_files.append(test_file)
+        else:
+          failed_test_files.append(test_file)
   return (test_section_title, success_count, test_count, passed_test_files, failed_test_files)
-     # print tests
 
 def clear_results():
   global test_count
