12f7cc4a3247e9efa21f656a0cd7f5afc4ac25b6
J <a@a.pl>
2012-11-18 13:22:17 +0100

    testy wykonywane alfabetycznie

--- runtests.py
+++ runtests.py
@@ -6,7 +6,7 @@ import subprocess as subp
 pass_on_success = "pass_on_success"
 pass_on_error = "pass_on_error"
 
-def run(test_section_title, testdir, subdir, test_pass_type, print_compiler_output = False, run_io_phase = True):
+def run(test_section_title, testdir, subdir, test_pass_type, print_compiler_output = True, run_io_phase = True):
   test_count = 0
   success_count = 0
   failed_test_files = []
@@ -17,7 +17,7 @@ def run(test_section_title, testdir, subdir, test_pass_type, print_compiler_outp
   for folder in os.walk(testdir + subdir):
     print "Folder:",folder[0]
     if len(folder[2]) > 0:
-      tests = set([x[0] for x in map(lambda x: x.split("."), folder[2])])
+      tests = sorted(list(set([x[0] for x in map(lambda x: x.split("."), folder[2])])))
       for t in tests:
         io_phase_succeeded = True
         compilation_phase_succeeded = False
@@ -30,38 +30,43 @@ def run(test_section_title, testdir, subdir, test_pass_type, print_compiler_outp
         test_input_path = folder[0] + "/" + input_file
         test_output_path = folder[0] + "/" + output_file
 
+        compiler_stdout = ""
+        compiler_stderr = ""
+        compiler_returncode = ""
+
+        test_info = []
+        
         if not os.path.exists(folder[0] + "/" + t + config.suffix_src):
-          print "Blad: nie istnieje plik zrodlowy testu",folder[0],t
+          test_info.append("Blad: nie istnieje plik zrodlowy testu "+folder[0]+" "+t)
         else:
-          print "Test:",test_src_path,
           compile_out = ""
           try:
-            compile_out = subp.check_output([config.compiler_bin, test_src_path], stderr = subp.STDOUT)
-            if test_pass_type == pass_on_success:
-              print "OK"
-              compilation_phase_succeeded = True
-            else:
-              print "FAILED"
-              compilation_phase_succeeded = False
+            compiler_process = subp.Popen([config.compiler_bin, test_src_path], stdout = subp.PIPE, stderr = subp.PIPE)
+            (compiler_stdout, compiler_stderr) = compiler_process.communicate()
+            stderr_lines = compiler_stderr.splitlines()
+
+            if compiler_process.returncode == 0:
+              if test_pass_type == pass_on_success:
+                if stderr_lines[0] == "OK":
+                  compilation_phase_succeeded = True
+                else:
+                  test_info.append("Kompilator nie wypisal \"OK\" w pierwszej linii stderr")
+            else: # non-zero return code
+              test_info.append("Kompilacja zakonczyla sie kodem bledu " + str(compiler_process.returncode))
+              if test_pass_type == pass_on_error:
+                if stderr_lines[0] == "ERROR":
+                  compilation_phase_succeeded = True
+                else:
+                  test_info.append("Kompilator nie wypisal \"ERROR\" w pierwszej linii stderr")
             if print_compiler_output:
-              print "Wyjscie z kompilacji:"
-              print compile_out
+              test_info.append("Wyjscie err z kompilacji:")
+              test_info.append(compiler_stderr)
+              test_info.append("Wyjscie std z kompilacji:")
+              test_info.append(compiler_stdout)
             print
           except OSError as ose:
-            print
-            print "OSError",ose
-          except subp.CalledProcessError as non_zero:
-            if test_pass_type == pass_on_error:
-              print "OK"
-              compilation_phase_succeeded = True
-            else:
-              print "FAILED"
-              compilation_phase_succeeded = False
-            print "Kompilacja zakonczyla sie z kodem bledu", non_zero.returncode
-            if print_compiler_output:
-              print "Wyjscie z kompilacji:"
-              print non_zero.output
-            print
+            test_info.append("OSError")
+            test_info.append(str(ose))
         # sprawdzanie i/o
         if os.path.exists(test_input_path) and run_io_phase:
           if not os.path.exists(test_output_path):
@@ -69,7 +74,7 @@ def run(test_section_title, testdir, subdir, test_pass_type, print_compiler_outp
             output = subp.check_output("test_bin_path" + " < " + test_input_path, shell=True)
             expected_output = file(test_output_path).read()
             if output != expected_output:
-              print "FAILED"
+              print "I/O TEST FAILED"
               print "oczekiwano:"
               print expected_output
               print "otrzymano:"
@@ -77,11 +82,16 @@ def run(test_section_title, testdir, subdir, test_pass_type, print_compiler_outp
               io_phase_succeeded = False
             else:
               io_phase_succeeded = True
+
+        print "Test:", test_src_path,
         if ((not run_io_phase) or io_phase_succeeded) and compilation_phase_succeeded:
           success_count = success_count + 1
+          print "OK"
           passed_test_files.append(test_file)
         else:
+          print "FAILED"
           failed_test_files.append(test_file)
+        print "\n".join(test_info)
   return (test_section_title, success_count, test_count, passed_test_files, failed_test_files)
 
 def clear_results():
@@ -96,6 +106,8 @@ def print_results():
 test_runs = [
  ("OFFICIAL GOOD", config.official_testdir, config.good_dir, pass_on_success),
  ("OFFICIAL BAD", config.official_testdir, config.bad_dir, pass_on_error),
+ ("JACEK GOOD", config.jackowy_testdir, config.good_dir, pass_on_success),
+ ("JACEK BAD", config.jackowy_testdir, config.bad_dir, pass_on_error),
  ("MINE GOOD", config.my_testdir, config.good_dir, pass_on_success),
  ("MINE BAD", config.my_testdir, config.bad_dir, pass_on_error)
 ]
